ddafbb8fdc6b2bac2e2d388ebcd076da0db4edd4
itslinotlie <michael.li.web@gmail.com>
2023-04-02 00:37:09 -0400

    Modify __init__ to no longer import *

--- parsley/__init__.py
+++ parsley/__init__.py
@@ -1,4 +1,13 @@
 from bitstring import BitString
-from fields import ASCII, Enum, Numeric
-from parsley_definitions import *
-from parsley import *
+from fields import ASCII, Enum, Numeric, Switch
+from parsley_definitions import (
+    MESSAGE_TYPE, BOARD_ID,
+    TIMESTAMP_2, TIMESTAMP_3,
+    BOARD_STATUS, FIELDS
+)
+from parsley import (
+    parse, parse_raw,
+    parse_live_telemetry,
+    parse_usb_debug,
+    parse_logger
+)
