a0b3de4a8903f8967e718497b45eaee7153a3658
Maisim Zaman <m8zaman@uwaterloo.ca>
2023-10-05 20:02:57 -0400

    Completed generative canlib python message_types.py to C

--- code_gen/create_message_types.py
+++ code_gen/create_message_types.py
@@ -28,7 +28,22 @@ def convert_message_types_to_c(msg_type, board_id, gen_cmd, actuator_states, arm
 
         key = python_line[0]
         value = python_line[1]
+     
         c_line = f"#define BOARD_ID_{key.strip().upper().replace(' ', '_'):<25} {value:#06X}\n"
+        
+
+        return c_line
+    
+    def convert_gen_cmd_to_c_string(python_line):
+
+        key = python_line[0]
+        value = python_line[1]
+        
+        if value == 0:    
+            c_line = f"    {key} = {value},"
+        else:
+            c_line = f"    {key},"
+        
         
 
         return c_line
@@ -67,14 +82,75 @@ def convert_message_types_to_c(msg_type, board_id, gen_cmd, actuator_states, arm
             
     c_file_path = "./code_genmessage_types.c"
     with open(c_file_path, "w") as c_file:
+       c_file.write("#ifndef MESSAGE_TYPES_H_" + "\n")
+       c_file.write("#define MESSAGE_TYPES_H_" + "\n")
+       c_file.write("\n")
        
        for python_line in msg_type_lines:
 
             c_file.write(convert_msg_type_to_c_string(python_line) + "\n")
 
        for python_line in  board_id_lines:
-
             c_file.write(convert_board_id_to_c_string(python_line) + "\n")
+            
+       for python_line in gen_cmd_lines:
+            if gen_cmd_lines.index(python_line) == 0:
+                c_file.write("enum GEN_CMD {" + "\n")
+            c_file.write(convert_gen_cmd_to_c_string(python_line) + "\n")
+            if gen_cmd_lines.index(python_line) == len(gen_cmd_lines)-1:
+                c_file.write("};" + "\n")
+                c_file.write("\n" )
+        
+       for python_line in actuator_states_lines:
+            if actuator_states_lines.index(python_line) == 0:
+                c_file.write("enum ACTUATOR_STATE {" + "\n")
+            c_file.write(convert_gen_cmd_to_c_string(python_line) + "\n")
+            if actuator_states_lines.index(python_line) == len(actuator_states_lines)-1:
+                c_file.write("};" + "\n")
+                c_file.write("\n" )
+                
+       for python_line in arm_states_lines:
+            if arm_states_lines.index(python_line) == 0:
+                c_file.write("enum ARM_STATE {" + "\n")
+            c_file.write(convert_gen_cmd_to_c_string(python_line) + "\n")
+            if arm_states_lines.index(python_line) == len(arm_states_lines)-1:
+                c_file.write("};" + "\n")
+                c_file.write("\n" )
+                
+       for python_line in board_status_lines:
+            if board_status_lines.index(python_line) == 0:
+                c_file.write("enum BOARD_STATUS {" + "\n")
+            c_file.write(convert_gen_cmd_to_c_string(python_line) + "\n")
+            if board_status_lines.index(python_line) == len(board_status_lines)-1:
+                c_file.write("};" + "\n" )
+                c_file.write("\n" )
+                
+       for python_line in sensor_id_lines:
+            if sensor_id_lines.index(python_line) == 0:
+                c_file.write("enum SENSOR_ID {" + "\n")
+            c_file.write(convert_gen_cmd_to_c_string(python_line) + "\n")
+            if sensor_id_lines.index(python_line) == len(sensor_id_lines)-1:
+                c_file.write("};" + "\n" )
+                c_file.write("\n" )
+                
+       for python_line in fill_direction_lines:
+            if fill_direction_lines.index(python_line) == 0:
+                c_file.write("enum FILL_DIRECTION {" + "\n")
+            c_file.write(convert_gen_cmd_to_c_string(python_line) + "\n")
+            if fill_direction_lines.index(python_line) == len(fill_direction_lines)-1:
+                c_file.write("};" + "\n" )
+                c_file.write("\n" )
+        
+       for python_line in actuator_id_lines:
+            if actuator_id_lines.index(python_line) == 0:
+                c_file.write("enum ACTUATOR_ID {" + "\n")
+            c_file.write(convert_gen_cmd_to_c_string(python_line) + "\n")
+            if actuator_id_lines.index(python_line) == len(actuator_id_lines)-1:
+                c_file.write("};" + "\n" )
+                c_file.write("\n" )
+
+       c_file.write("#endif // compile guard" + "\n")
+            
             
         
             
