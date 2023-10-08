ac0a390ca0ed51c3b2aedf44a8b1e8e077a6fc3f
Maisim Zaman <m8zaman@uwaterloo.ca>
2023-10-08 16:08:01 -0400

    Optimized Generative Canlib code and cut down on copy-paste code

--- parsley/create_message_types.py
+++ parsley/create_message_types.py
@@ -1,43 +1,25 @@
-import sys
-from parsley.message_types import msg_type, board_id, gen_cmd, actuator_states, arm_states, board_status, logger_error, sensor_id, fill_direction, actuator_id
-from parsley.message_definitions import MESSAGES
+from parsley.message_definitions import MESSAGES, MESSAGE_TYPE
 from parsley.fields import Enum
 
-def convert_message_types_to_c():
-    msg_type_lines = []
-    board_id_lines = []
-    gen_cmd_lines = []
-    actuator_states_lines = []
-    arm_states_lines = []
-    board_status_lines = []
-    logger_error_lines = []
-    sensor_id_lines = []
-    fill_direction_lines = []
-    actuator_id_lines = []
+
+def convert_message_types_to_c(c_file_path="./code_genmessage_types.c"):
     
-    s1 = set()
+    s1 = set(['board_id'])
     data_dicts = []
+    define_type_dicts = []
     
-    
-    
-    enum_data = [
-        {'lines': gen_cmd_lines, 'enum_name': 'GEN_CMD'},
-        {'lines': actuator_states_lines, 'enum_name': 'ACTUATOR_STATE'},
-        {'lines': arm_states_lines, 'enum_name': 'ARM_STATE'},
-        {'lines': board_status_lines, 'enum_name': 'BOARD_STATUS'},
-        {'lines': sensor_id_lines, 'enum_name': 'SENSOR_ID'},
-        {'lines': fill_direction_lines, 'enum_name': 'FILL_DIRECTION'},
-        {'lines': actuator_id_lines, 'enum_name': 'ACTUATOR_ID'}
-    ]
-    
-    
+    define_type_dicts.append({'data_dict':MESSAGE_TYPE.map_key_val, 'lines_list':[], 'enum_name': MESSAGE_TYPE.name})
+
     for k, v in MESSAGES.items():
         for f in v:
             if isinstance(f, Enum):
+                if f.name == 'board_id':
+                    define_type_dicts.append({'data_dict':f.map_key_val, 'lines_list':[], 'enum_name': f.name})
+                    
+                    
                 if f.name not in s1:
                     s1.add(f.name)
                     data_dicts.append({'data_dict':f.map_key_val, 'lines_list':[], 'enum_name': f.name})
-    
     
     
     def convert_msg_type_to_c_string(python_line, type):
@@ -49,7 +31,6 @@ def convert_message_types_to_c():
         else:
             c_line = f"#define BOARD_ID_{key.strip().upper().replace(' ', '_'):<25} {value:#06X}\n"
         
-
         return c_line
     
     
@@ -63,8 +44,6 @@ def convert_message_types_to_c():
         else:
             c_line = f"    {key},"
         
-        
-
         return c_line
     
     for data_entry in data_dicts:
@@ -72,20 +51,21 @@ def convert_message_types_to_c():
             data_entry['lines_list'].append((key, value))
     
         
-    c_file_path = "./code_genmessage_types.c"
     with open(c_file_path, "w") as c_file:
        c_file.write("#ifndef MESSAGE_TYPES_H_" + "\n")
        c_file.write("#define MESSAGE_TYPES_H_" + "\n")
        c_file.write("\n")
        
-       for python_line in msg_type_lines:
-            c_file.write(convert_msg_type_to_c_string(python_line,  "MSG") + "\n")
-
-       for python_line in  board_id_lines:
-            c_file.write(convert_msg_type_to_c_string(python_line, 'BOARD') + "\n")
+       for dict in range(len(define_type_dicts)):
+       
+        for key, value in define_type_dicts[dict]['data_dict'].items():
+            if dict == 0:
+                c_file.write(convert_msg_type_to_c_string((key, value),  "MSG") + "\n")
+            elif dict == 1:
+                c_file.write(convert_msg_type_to_c_string((key, value), 'BOARD') + "\n")
             
-       for enum_info in enum_data:
-            lines = enum_info['lines']
+       for enum_info in data_dicts:
+            lines = enum_info['lines_list']
             enum_name = enum_info['enum_name']
             
             c_file.write(f"enum {enum_name} {{\n")
@@ -96,7 +76,6 @@ def convert_message_types_to_c():
                 if idx == len(lines) - 1:
                     c_file.write("};\n\n")
                 
-
        c_file.write("#endif // compile guard" + "\n")
             
             
