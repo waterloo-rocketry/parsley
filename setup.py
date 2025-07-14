556227d56afc1eafbf77cf290961f564b7224992
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-07-14 11:10:44 -0400

    Bump version number to 2025.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 setup(
     name='parsley',
-    version='2025.3',
+    version='2025.4',
     description='Library to transcode CAN messages and human-readable text',
     long_description=long_description,
     long_description_content_type='text/markdown',
