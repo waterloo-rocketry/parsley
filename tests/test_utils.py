8131d4016c19eef63212d3f2e8ae3049f179a540
Shrey <shreyshingala1@gmail.com>
2025-09-30 22:49:13 -0400

    Updated test_utils.py to include the new sid format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,31 @@
 import pytest
 
-from bitstring import BitString
-from message_definitions import MESSAGE_TYPE, BOARD_ID, MESSAGE_SID
+from parsley.bitstring import BitString
+from parsley.message_definitions import MESSAGE_TYPE, BOARD_TYPE_ID, BOARD_INST_ID, MESSAGE_SID, MESSAGE_PRIO
 
 FLOAT_TOLERANCE = 0.01
 
 def approx(value):
     return pytest.approx(value, FLOAT_TOLERANCE)
 
+
+def create_msg_sid_from_strings(priority_str: str, msg_type_str: str, reserved_str: str, board_type_str: str, board_inst_str: str):
+    (msg_prio_bits, _) = MESSAGE_PRIO.encode(priority_str)
+    (msg_type_bits, _) = MESSAGE_TYPE.encode(msg_type_str)
+    (reserved_bits, _) = bytes([0, 0]), 2 # reserved bits are always 0
+    (board_type_bits, _) = BOARD_TYPE_ID.encode(board_type_str)
+    (board_inst_bits, _) = BOARD_INST_ID.encode(board_inst_str)
+    
+    bit_msg_sid = BitString(msg_prio_bits, MESSAGE_PRIO.length)
+    bit_msg_sid.push(msg_type_bits, MESSAGE_TYPE.length)
+    bit_msg_sid.push(reserved_bits, 2)
+    bit_msg_sid.push(board_type_bits, BOARD_TYPE_ID.length)
+    bit_msg_sid.push(board_inst_bits, BOARD_INST_ID.length)
+    msg_sid = bit_msg_sid.pop(MESSAGE_SID.length)
+    return msg_sid
+
+# Old method left here for reference (still used in some tests and need to change to new method)
+''' 
 def create_msg_sid_from_strings(msg_type_str: str, board_id_str: str):
     (msg_type_bits, _) = MESSAGE_TYPE.encode(msg_type_str)
     (board_id_bits, _) = BOARD_ID.encode(board_id_str)
@@ -15,3 +33,4 @@ def create_msg_sid_from_strings(msg_type_str: str, board_id_str: str):
     bit_msg_sid.push(board_id_bits, BOARD_ID.length)
     msg_sid = bit_msg_sid.pop(MESSAGE_SID.length)
     return msg_sid
+'''
