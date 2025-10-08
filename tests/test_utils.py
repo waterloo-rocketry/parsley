fab130314b0b02dc622dc79ca98323b2bdcf12c8
Shrey <shreyshingala1@gmail.com>
2025-10-07 20:30:22 -0400

    Quick comment

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ import utils as utilities
 def test_helpers_create_msg_sid_roundtrip():	
 	sid = utilities.create_msg_sid_from_strings('HIGH', 'GENERAL_BOARD_STATUS', '0', 'RLCS_RELAY', 'PRIMARY')
 	
-	expected_bytes_len = (MESSAGE_SID.length + 7) // 8
+	expected_bytes_len = (MESSAGE_SID.length + 7) // 8 # rounds up to nearest byte
  
 	assert isinstance(sid, (bytes, bytearray))
 	assert len(sid) == expected_bytes_len
